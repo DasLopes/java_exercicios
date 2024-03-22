bf68cdda486e2fb70caf4700d8655b2d74bb91f7
daslopes <daslopes@outlook.com.br>
2024-03-22 09:53:17 -0300

    exercício matrix 3x3, mostrando números na diagonal e negativos

---
+++ matrix1/src/application/Program.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int[][] matrix = new int[n][n];
+		
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[i].length; j++) {
+				matrix[i][j] = sc.nextInt();
+			}
+		}
+		
+		System.out.println("Main diagonal:");
+		for(int i = 0; i< matrix.length; i++) {
+			System.out.print(matrix[i][i] + " ");
+		}
+		
+		int count = 0;
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[i].length; j++) {
+				if(matrix[i][j] < 0) {
+					count++;
+				}
+			}
+		}
+		System.out.println();
+		System.out.println("Negative numbers = " + count);
+		
+		sc.close();
+	}
+
+}
